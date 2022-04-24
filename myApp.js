68e956d35706772261c71c2cc7ed167555dd627a
Sharif Jubayer Arefin <contact@jubayerarefin.info>
2022-04-24 16:37:35 +0000

    Fixes creating new schema

--- myApp.js
+++ myApp.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 
 mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
 
-const personSchema = new mongoose.schema({
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
     name : { type: String, required: true },
     age :  { type: Number },
     favoriteFoods : [{type: String}]
