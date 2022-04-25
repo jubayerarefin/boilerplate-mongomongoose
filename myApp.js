f15cb372576ddb021b8e7c7e18989a427e58c157
Sharif Jubayer Arefin <contact@jubayerarefin.info>
2022-04-25 16:54:12 +0000

    Implements findPeopleByName

--- myApp.js
+++ myApp.js
@@ -36,8 +36,13 @@ const createManyPeople = (arrayOfPeople, done) => {
     });
 };
 
+var personName = "Jubayer";
+
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+    Person.find({name:personName}, function(err, data){
+        if (err) return console.error(err);
+        done(null, data); 
+    });
 };
 
 const findOneByFood = (food, done) => {
